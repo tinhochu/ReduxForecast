c66d68aca1142dfd084a88931491ae6b52a3f027
Tin Chu <mchu.tinho@gmail.com>
2017-11-07 09:17:26 -0600

    added City column in table, reducer got completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '1ae44021726610cd688d3d9262a2821b';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -9,8 +9,6 @@ export function fetchWeather( city ) {
 
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get( url );
-	
-	console.log('Request: ', request);
 
 	return {
 		type   : FETCH_WEATHER,
